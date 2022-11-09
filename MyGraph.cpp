c82f7ef1afdcb5cbe101c2a74b443e8ec07697f2
Benjamín Gutierrez Mendoza <benjamingutierrezmendoza@MacBook-Pro-de-Benjamin.local>
2022-11-09 11:25:33 -0600

    version final

--- MyGraph.cpp
+++ MyGraph.cpp
@@ -12,9 +12,9 @@ void MyGraph::load(std::vector<std::vector<int>>& vecOfInt){
     this -> vecOfInt = vecOfInt;
     numVertex = vecOfInt.size();
     numEdges = 0;
-    for (int i = 0; i < numVertex; i++){
-        for (int j = 0; j < numVertex; j++) {
-            if (vecOfInt[i][j] == 1) {
+    for(int i = 0; i < numVertex; i++){
+        for(int j = 0; j < numVertex; j++){
+            if(vecOfInt[i][j] == 1) {
                 numEdges++;
             }
         }
@@ -24,18 +24,18 @@ void MyGraph::load(std::vector<std::vector<int>>& vecOfInt){
 
 //Complejidad O(n^2) 
 void MyGraph::DFS(int initial){
-    std::vector<bool> visited(numVertex, false);
-    std::vector<int> stack;
-    stack.push_back(initial);
-    while (!stack.empty()){
-        int v = stack.back();
-        stack.pop_back();
-        if (!visited[v]) {
-            visited[v] = true;
-            std::cout << v << " ";
+    std::vector<bool> ifVisit(numVertex, false);
+    std::vector<int> pile;
+    pile.push_back(initial);
+    while (!pile.empty()){
+        int conf = pile.back();
+        pile.pop_back();
+        if (!ifVisit[conf]) {
+            ifVisit[conf] = true;
+            std::cout << conf << " ";
             for (int i = numVertex - 1; i >= 0; i--){
-                if (vecOfInt[v][i] == 1 && !visited[i]){
-                    stack.push_back(i);
+                if (vecOfInt[conf][i] == 1 && !ifVisit[i]){
+                    pile.push_back(i);
                 }
             }
         }
@@ -45,18 +45,18 @@ void MyGraph::DFS(int initial){
 
 //Complejidad O(m+n) 
 void MyGraph::BFS(int initial){
-    std::vector<bool> visited(numVertex, false);
-    std::vector<int> queue;
-    queue.push_back(initial);
-    while (!queue.empty()){
-        int v = queue.front();
-        queue.erase(queue.begin());
-        if (!visited[v]) {
-            visited[v] = true;
-            std::cout << v << " ";
+    std::vector <bool> ifVisit(numVertex, false);
+    std::vector <int> intOfQueue;
+    intOfQueue.push_back(initial);
+    while (!intOfQueue.empty()){
+        int conf = intOfQueue.front();
+        intOfQueue.erase(intOfQueue.begin());
+        if (!ifVisit[conf]) {
+            ifVisit[conf] = true;
+            std::cout << conf << " ";
             for (int i = 0; i < numVertex; i++){
-                if (vecOfInt[v][i] == 1 && !visited[i]){
-                    queue.push_back(i);
+                if (vecOfInt[conf][i] == 1 && !ifVisit[i]){
+                    intOfQueue.push_back(i);
                 }
             }
         }
--- main.cpp
+++ main.cpp
@@ -10,24 +10,22 @@ Benjamin Gutierrez Mendoza - A01642356
 using namespace std;
 
 int main() {
-    vector <vector<int>> mat = {
+    //Inizializacion de la matriz
+    vector <vector<int>> matrixOfPositions = {
         {0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
         {1, 0, 0, 1, 1, 0, 0, 0, 0, 0},
         {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0, 0, 1, 1, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
-        {0, 0, 0, 0, 1, 1, 1, 1, 1, 0}
+        {0, 1, 0, 0, 0, 0, 0, 1, 1, 0}
     };
-    MyGraph g(mat);
+    //Creacion del objeto en grafo
+    MyGraph newGraph(matrixOfPositions);
     cout << "Impresioes del grafo" << endl;
-    cout << "DFS: ";
-    g.DFS(0);
-    cout << "BFS: ";
-    g.BFS(0);
+    cout << "En DFS: ";
+    //uso de DFS
+    newGraph.DFS(0);
+    cout << "En BFS: ";
+    //uso de BFS
+    newGraph.BFS(0);
     return 0;
 }
 
